8595390fd3bbb5a4a22cf24e8cd88df3b893e9da
manh.th200298 <manhtran98bg@gmail.com>
2021-03-26 21:21:04 +0700

    Change main.c

--- ATMEGA8_Blink/main.c
+++ ATMEGA8_Blink/main.c
@@ -5,10 +5,14 @@
  *      Author: manht
  */
 #include <avr/io.h>
+#include <util/delay.h>
 int main()
 {
+	DDRC = (1<<PC0);
+	PORTC = (1<<PC0);
 	while (1)
 	{
-
+		PORTC ^=(1<<PC0);
+		_delay_ms(1000);
 	}
 }
